98427a3aca25c69dbe6fdc4465292f300335eec7
DesadaLleshanaku <88627951+DesadaLleshanaku@users.noreply.github.com>
2023-07-03 15:25:07 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+<div class="footerbottom">
+    <div class="container">
+        Copyright &copy; <?php echo date("Y").' <a href="'.$GLOBALS['baseurl'].'">'.$GLOBALS['projectname'].'</a>'; ?> <br/> Malla Technologies
+    </div>
+</div>
+<div class="extrafooter">
+    <a href="tel:(07) 3040 6264"><strong>Call now:</strong> (07) 3040 6264</a>
+</div>
---
+++ header.php
@@ -0,0 +1,17 @@
+<div class="header">
+    <div class="header-main">
+        <div class="container">
+            <div class="ui grid topheader">
+                <div class="three wide column">
+                    <i class="small content icon" id="mainmenu"></i>
+                </div>
+                <div class="ten wide column center aligned">
+                        <img src="<?php echo $GLOBALS['baseurl']; ?>images/design/Malla-Technologies-Logo.png" alt="Malla Technologies Logo" class="mainlogo" />
+                </div>
+                <div class="three wide column right aligned">
+                    <a href="contact" style="color:#FFF;"><i class="small envelope icon" id="settings"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
